cd1a6d403b6028f3d54cdf10191eaf245c98c818
Ryan Allen <allenryan14@gmail.com>
2020-08-12 19:35:30 -0400

    Add alerts for crud success/error

--- leadmanager/frontend/src/actions/leads.js
+++ leadmanager/frontend/src/actions/leads.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
-
-import { GET_LEADS, ADD_LEAD, DELETE_LEAD } from './types';
+import { createMessage } from './messages'
+import { GET_LEADS, ADD_LEAD, DELETE_LEAD, GET_ERRORS } from './types';
 
 export const getLeads = () => dispatch => {
     axios.get('/api/leads/')
@@ -16,17 +16,32 @@ export const getLeads = () => dispatch => {
 export const addLead = (lead) => dispatch => {
     axios.post('/api/leads/', lead)
         .then(res => {
+            dispatch(createMessage({
+                leadAdded: 'Lead added'
+            }))
             dispatch({
                 type: ADD_LEAD,
                 payload: res.data
             })
         })
-        .catch(err => console.error(err));
+        .catch(err => {
+            const errors = {
+                msg: err.response.data,
+                status: err.response.status
+            };
+            dispatch({
+                type: GET_ERRORS,
+                payload: errors
+            })
+        });
 }
 
 export const deleteLead = (id) => dispatch => {
     axios.delete(`/api/leads/${id}/`)
         .then(res => {
+            dispatch(createMessage({
+                leadDeleted: 'Lead deleted'
+            }))
             dispatch({
                 type: DELETE_LEAD,
                 payload: id
